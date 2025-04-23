510689d97d6886549e422a6aa0f0a25da61aa84d
noxyees <noxyees@gmail.com>
2025-04-23 12:07:43 +0200

    NoteApp#4: Add transaction for mongodb

--- services/task/src/main/kotlin/com/noxyee/task/TaskApplication.kt
+++ services/task/src/main/kotlin/com/noxyee/task/TaskApplication.kt
@@ -3,8 +3,12 @@ package com.noxyee.task
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
+@EnableMongoAuditing
+@EnableTransactionManagement
 @EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
